c3e88f183fe1b9d77d3a26e1771b5a4231a51a1b
Rohan Tamhankar <curiousrohan1@gmail.com>
2022-01-08 19:01:59 -0800

    Signing off for Saturday.

---
+++ src/index.js
@@ -0,0 +1,46 @@
+
+
+function signUp(){
+    var email = document.getElementById("email-sign-up").value;
+    var password = document.getElementById("password-sign-up").value;
+    var repeatPassword = document.getElementById("repeat-password").value;
+    var isEligible = document.getElementById("agree").checked;
+    isEligible = isEligible&& (password===repeatPassword);
+    if(!isEligible){
+        showhide(true,"warning-sign-up");
+        if(!document.getElementById("agree").checked){
+            document.getElementById("warning-sign-up").innerText = "Sorry, you must agree to the Terms of Service and Privacy Policy.";
+            console.error("Sorry, you must agree to the Terms of Service and Privacy Policy.")
+        }
+        else{
+            document.getElementById("warning-sign-up").innerText = "Sorry, your passwords do not match.";
+            console.error("Sorry, your passwords do not match.");
+        }
+    }
+    else{
+        showhide(false,"warning-sign-up");
+//    Do POST
+        document.location.href="home.html";
+    }
+}
+function signIn(){
+    var email = document.getElementById("email-sign-in").value;
+    var password = document.getElementById("password-sign-in").value;
+    // GET whether or not password is actualPassWd
+    {//if isn't 300:
+        showhide(true,"warning-sign-in");
+        document.getElementById("warning-sign-in").innerText = "Incorrect password.";
+        console.error("Incorrect password.")
+    }
+    //else
+    showhide(false,"warning-sign-in");
+}
+
+function showhide(show,id){
+    if(show){
+        document.getElementById(id).style.display = "block";
+    }else{
+        document.getElementById(id).style.display = "none";
+    }
+}
+
